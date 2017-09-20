a4c0864c99aceb2a33f523935bb8a989ceccf9b2
A-1ex <alexndr.fedoseev@aol.com>
2017-09-20 21:05:32 +0300

    Adds scripts for menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
 // JS Goes here - ES6 supported
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.querySelector("video");
+  const menuBtn = document.getElementById("menu_btn");
+  const body = document.body;
+
+  video.playbackRate = 0.6;
+  menuBtn.addEventListener("click", ($event) => {
+    body.classList.toggle("is-menu");
+  });
+
+});
